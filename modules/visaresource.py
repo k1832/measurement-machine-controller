fb17141672a64341045156014628f9d835a80f42
Keita Morisaki <kmta1236@gmail.com>
2021-12-13 14:00:47 +0900

    add "print_connected_resources()"

--- modules/visaresource.py
+++ modules/visaresource.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from typing import Any
@@ -7,18 +8,22 @@ import atexit
 
 @dataclass
 class VisaResource:
-    addr: InitVar[str]
+    addr: InitVar[str] = None
     visa_path: InitVar[str] = "C:\\Windows\\system32\\visa64.dll"
     rm: Any = field(init= False)
-    resource: Any = field(init= False)
+    resource: Any = field(init=False)
 
     def __post_init__(self, addr, visa_path):
         self.rm = visa.ResourceManager(visa_path)
-        # Always close the resource at exit
         atexit.register(self.close_resource)
-        self.resource = self.rm.open_resource(addr)
-        self.resource.timeout = 10 * 1000
-        print("DEVICE:", self.resource.query("*IDN?"))
+        if addr:
+            self.resource = self.rm.open_resource(addr)
+            self.resource.timeout = 10 * 1000
+            print("DEVICE:", self.resource.query("*IDN?"))
+    
     def close_resource(self):
         print("Closing the resource manager...")
         self.rm.close()
+
+    def print_connected_resources(self):
+        print(self.rm.list_resources())
